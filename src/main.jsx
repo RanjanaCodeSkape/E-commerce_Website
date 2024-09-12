a492b2658ba27aea7ae3259f49c03a845ceeffac
apple <apple@apples-MacBook-Air.local>
2024-09-12 15:15:46 +0530

    new changes and removed ssr

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,9 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId='631612017155-9g8tab51m7s1u71kkv7ocgjk2hqanvrm.apps.googleusercontent.com'>
-   
     <Router>
     <App />
     </Router>
     </GoogleOAuthProvider>
- 
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname ,path} from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-});
+})
